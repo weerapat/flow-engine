75e7f30b97dba5f3a1a4039aace6abc419146a72
Weerapat Supap <weerapatsu@gmail.com>
2017-11-13 22:13:36 +0700

    Add flow logic test

---
+++ src/__tests__/FlowStore.test.js
@@ -0,0 +1,43 @@
+import FlowStore from '../FlowStore';
+
+describe('Flow logic', () => {
+  const flow = new FlowStore();
+
+  test('flow has initial rules after creation', () => {
+    expect(flow.store.rules).toHaveLength(3);
+  });
+
+  test('test add new rule', () => {
+    expect(flow.addRule({
+      title: 'New rule',
+      body: `function (obj) {
+            return obj.pet === 'boo' && obj.size === 2;
+          }`
+    })).toHaveLength(4);
+  });
+
+  test('test remove rule', () => {
+    expect(flow.removeRule({
+      id: 2,
+      title: 'Rule 2',
+      body: `function (obj) {
+            return obj.pet === 'cat' && obj.size === 12;
+          }`,
+      true_id: null,
+      false_id: 3
+    })).toHaveLength(3);
+  });
+
+  test('test flow execution', () => {
+    let results = flow.executeFlow({
+      pet: 'dog',
+      size: 10
+    });
+
+    expect(results).toHaveLength(4);
+    expect(results[0].status).toEqual('passed');
+    expect(results[1].status).toEqual('failed');
+    expect(results[2].status).toEqual('failed');
+    expect(results[results.length - 1].title).toEqual('End');
+  });
+});
